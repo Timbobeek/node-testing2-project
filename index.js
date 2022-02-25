9a2685ccd6ce204041912079c2e1c26873eafb84
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-24 21:03:55 -0600

    npm run start works

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//hdhdhdhdh
+require('dotenv').config()
+
+const server = require('./api/server')
+const port = process.env.PORT || 6666
+
+server.listen(port, () => console.log(`\n API running on port ${port}\n`))
